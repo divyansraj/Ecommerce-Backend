1c90b2333a135ec365569d390d69b68d1a489435
divyansraj <divyanshu10245@gmail.com>
2024-06-17 05:29:31 +0530

    deploying error 5

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const path = require("path");
 const express = require("express");
 require("dotenv").config();
-const helmet = require("helmet"); // Helmet helps you secure your Express apps by setting various HTTP headers.
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
@@ -9,50 +13,41 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'none'"],
-      fontSrc: ["'self'", "data:"], // Allow fonts to be loaded from the same origin and data URIs
-      // Add other directives as needed
+      fontSrc: ["'self'", "data:"],
     },
   })
 );
 
-const morgan = require("morgan");
-const cookieParser= require("cookie-parser");
-const fileUpload = require("express-fileupload");
-
-//for swagger documentation
-// const swaggerUi= require("swagger-ui-express");
-// const YAML = require("yamljs");
-// const swaggerDocument = YAML.load("./swagger.yaml");
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.set("view engine", "ejs");
 
-//regular middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+// Regular middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//cookie and file middlewares
+// Cookie and file middlewares
 app.use(cookieParser());
-app.use(fileUpload({
-    useTempFiles:true,
-    tempFileDir:"/temp/",
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.resolve(__dirname, "tmp"), // Use 'tmp' instead of 'temp'
+  })
+);
 
-//morgan middleware
+// Morgan middleware
 app.use(morgan("tiny"));
 
-// importing all the routes here
-
+// Importing all the routes here
 const home = require("./routes/home");
 const user = require("./routes/user");
 const product = require("./routes/product");
 
-//router middleware
-app.use('/api/v2',home);
-app.use('/api/v2',user);
-app.use('/api/v2',product);
-app.use('/post',(req,res) => {
-    res.render("postform")
-})
+// Router middleware
+app.use("/api/v2", home);
+app.use("/api/v2", user);
+app.use("/api/v2", product);
+app.use("/post", (req, res) => {
+  res.render("postform");
+});
 
-//export app.js
+// Export app.js
 module.exports = app;
